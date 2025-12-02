d215a950153a7f994be9afd9789a4db0646bf598
Emmanuel <emmanuelbaraka572@gmail.com>
2025-12-02 23:16:52 +0300

    Modified quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 
 function checkAnswer () {
     const correctAnswer = '4';
-    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
  
     const feedback = document.getElementById("feedback");
   if (userAnswer == correctAnswer) {
